bd9469ac40ecd221d8c16eea699dd5e1886bbb11
Evan Clough & Austin Kincaid <student@epicodus.com>
2015-09-24 14:19:34 -0700

    Add functionality to make and list comments.

--- app/models/post.js
+++ app/models/post.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   image: DS.attr(),
   title: DS.attr(),
-  caption: DS.attr()
+  caption: DS.attr(),
+  comments: DS.hasMany('comment', { async: true }),
 });
--- app/routes/post.js
+++ app/routes/post.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('post', params.post_id);
   },
+  actions: {
+    saveComment(params) {
+      var newComment = this.store.createRecord('comment', params);
+      newComment.save();
+      params.post.save();
+      this.transitionTo('post', params.post);
+    },
+  }
 });
