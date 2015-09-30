85ebb6d3deeed89ce5ac45434be72e15abd10086
rachael berecka & evan clough <student@epicodus.com>
2015-09-30 10:49:36 -0700

    Add attributes for place.

--- app/components/new-place.js
+++ app/components/new-place.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewPlace: false,
+  actions: {
+    placeFormShow() {
+      this.set('addNewPlace', true);
+    },
+    save() {
+      var params = {
+        latitue: this.get('latitude')
+        title: this.get('title'),
+        image: this.get('image'),
+        name: this.get('name'),
+        type: this.get('type'),
+        hours: this.get('hours'),
+        notes: this.get('notes'),
+        author: this.get('author'),
+        openDate: this.get('openDate'),
+        closeDate: this.get('closeDate'),
+        tag: this.get('tag')
+      };
+      this.set('addNewPlace', false),
+      this.sendAction('save', params);
+    }
+  }
 });
--- app/models/place.js
+++ app/models/place.js
@@ -1,5 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  latitude: DS.attr('number'),
+  longtitude: DS.attr('number'),
+  image: DS.attr(),
+  name: DS.attr(),
+  type: DS.attr(),
+  hours: DS.attr(),
+  notes: DS.attr(),
+  author: DS.attr(),
+  openDate: DS.attr(),
+  closeDate: DS.attr(),
+  comments: DS.hasMany('comment', { async: true }),
+  tag: DS.belongsTo('tag', { async: true })
 });
