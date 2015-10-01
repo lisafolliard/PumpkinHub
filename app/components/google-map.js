3fa7b53932e407b658929ef2bde2f8c7cc45530a
evan clough & rachael berecka <student@epicodus.com>
2015-10-01 14:12:20 -0700

    custom pumpkin marker

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -12,12 +12,21 @@ export default Ember.Component.extend({
         center: this.get('map').center(lat, lng),
         zoom: 15
       };
+      var pumpkinMarker = new google.maps.MarkerImage(
+        'assets/images/logo.png',
+        null, /* size is determined at runtime */
+        null, /* origin is 0,0 */
+        null, /* anchor is bottom center of the scaled image */
+        new google.maps.Size(25, 25)
+      );
 
       var newMap = this.get('map').findMap(container, options);
+
 
       var marker = new google.maps.Marker({
         position: myLatLng,
         map: newMap,
+        icon: pumpkinMarker
       });
     }
   }
