d6ce1f8eb9ac24d198bfd29a71628a65f604c72f
Janice Dunn & Lisa Folliard <student@epicodus.com>
2015-10-07 11:22:55 -0700

    Delete pantry item.

--- app/components/pantry-tile.js
+++ app/components/pantry-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(pantry) {
+      if (confirm('Are you sure you want to delete this pantry item?')) {
+        this.sendAction('destroyPantry', pantry);
+      }
+    }
+  }
 });
--- app/routes/pantry.js
+++ app/routes/pantry.js
@@ -4,11 +4,16 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('pantry');
   },
-  
+
   actions: {
     savePantry(params) {
       var newPantry = this.store.createRecord('pantry', params);
       newPantry.save();
+      this.transitionTo('pantry');
+    },
+
+    destroyPantry(pantry) {
+      pantry.destroyRecord();
       this.transitionTo('pantry');
     }
   }
