f6f45ae388687bfb6a71c1957a6861b2c98e02c8
Janice Dunn & Lisa Folliard <student@epicodus.com>
2015-10-07 11:14:34 -0700

    add new pantry form

--- app/routes/pantry.js
+++ app/routes/pantry.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('pantry');
+  },
+  
+  actions: {
+    savePantry(params) {
+      var newPantry = this.store.createRecord('pantry', params);
+      newPantry.save();
+      this.transitionTo('pantry');
+    }
+  }
 });
